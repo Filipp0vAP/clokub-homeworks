ad6e9e1643efe561be1c832addefb5508586daca
Aleksandr Filippov <Filipp0vAP@protonmail.com>
2023-12-28 01:08:47 +0300

    add yes to apt commands

--- 14.2/install-requariments.sh
+++ 14.2/install-requariments.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 
-apt-get update   
-apt-get install apt-transport-https ca-certificates   
-mkdir /etc/apt/keyrings/   
+apt-get update -y
+apt-get install apt-transport-https ca-certificates -y
+mkdir /etc/apt/keyrings/
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
-apt-get update 
+apt-get update -y
 apt install kubelet kubeadm kubectl containerd
 apt-mark hold kubelet kubeadm kubectl
 
