b490c3681f4a1ccf4363d425bee23e6a29d571dd
Aleksandr Filippov <Filipp0vAP@protonmail.com>
2023-12-28 01:12:32 +0300

    add one more yes

--- 14.2/install-requariments.sh
+++ 14.2/install-requariments.sh
@@ -6,7 +6,7 @@ mkdir /etc/apt/keyrings/
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update -y
-apt install kubelet kubeadm kubectl containerd
+apt install kubelet kubeadm kubectl containerd -y
 apt-mark hold kubelet kubeadm kubectl
 
 modprobe br_netfilter
